30c3b0e264a015369ee752bf5a4c3ed668262ba5
じん <gotwo525@gmail.com>
2020-10-12 20:14:01 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 'use strict';
+function fib(n){
+if(n === 0){
+    return 0;
+}else if(n === 1){
+    return 1;
+} 
+//return null;
+//
+return fib(n-1) + fib(n-2);
+}
+for(let i =0; i<=40; i++){
+    console.log( i + "のフィボナッチ数は"+ fib(i) +"です");
+}
+//     if(n === 0){
+//         return 0;
+//     }else if (n === 1){
+//         return 1;
+//     }
+//     return null;
+//     //console.log(fib(n))
+// }
